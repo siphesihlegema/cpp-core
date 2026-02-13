82bb5375da3d5f92ae47ec767569ac91c7ac446c
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-02-13 22:51:52 +0200

    Complile to one

---
+++ multiple/add.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int add(int x = 0, int y = 0) {
+    return x + y;
+}
---
+++ multiple/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int add(int x = 0, int y = 0);
+
+int main() {
+    std::cout << add(2, 3) << "\n";
+    return 0;
+}
