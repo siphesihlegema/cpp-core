bd886549c8da73aadaffaa9a0310d2bbb72c72dd
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-02-14 16:18:04 +0200

    using

---
+++ namespace/standerd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout<<"Hello"<<"\n";
+    return 0;
+}
+
+/*
+So basicly when we specify a library we simple to all the function 
+of that library to the global scope. this i suppose is not good and possibly not efficient
+and safe because i might cause nameing conflicts
+*/
