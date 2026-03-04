6a15fc71a3ac2c200b9dc1cdc0f583f709390bff
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-03-04 23:50:51 +0200

    Update linker and add literals

--- literals/literals.cpp
+++ literals/literals.cpp
@@ -3,6 +3,9 @@ A literal also known as a literal constant is a fixed value
 that has been inserted directly into the source code.
 */
 
+// Hello this is Sphe from the future 04/03/2026
+// im here t add on what i know about literals
+
 // Examples
 
 #include <iostream>
@@ -10,6 +13,9 @@ that has been inserted directly into the source code.
 int main() {
     std::cout << "this string is a literal" << '\n';
     std::cout << 5 << '\n'; // print the value of a literal
+    
+    // Scientific notation for floating point literals
+    std::cout << 2e2 << "\n";
 
     
     return 0;
