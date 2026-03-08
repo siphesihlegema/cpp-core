8b081ca4206d6b6502422349f4b7a2a01f80c729
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-03-08 03:43:43 +0200

    job well done

---
+++ enums/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+enum num {
+    A, B, C
+};
+
+int main() {
+    num nums = C;
+    std::cout << nums << "\n";
+}
--- strings/stdString.cpp
+++ strings/stdString.cpp
@@ -5,4 +5,11 @@ using namespace std;
 int main() {
     string name = "Bob";
     cout << name.max_size() << "\n";
+
+    // string lenght
+    std::cout << name.length() << "\n";
+    // the legth() function is a special function.
+    // Its a member function
+    
+    // With normal functions, we call function(object). With member functions, we call object.function().
 }
