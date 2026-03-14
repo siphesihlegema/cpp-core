0ff60fe05c7904011fa4a75dbbd0ecc8e273baf9
Siphesihle Gema <siphesihlegema30@gmail.com>
2026-03-14 03:02:33 +0200

    point me to my bed

--- pointers/main.cpp
+++ pointers/main.cpp
@@ -2,12 +2,33 @@
 
 #define LOG(x) std::cout << x << std::endl
 
+// A pointer is an object that holds a memory address (typically of another variable) as its value.
+// This allows us to store the address of some other object to use later.
+
 int main() {
+    // so in pointers i introduce you to &(address-of operator) operator. it is used to get the memory address of it operand.
 
-    int var = 8;
-    int* ptr = &var;
+    int var = 2;
+    int* ptre = &var;
 
-    LOG(ptr);
-    *ptr = 100;
-    LOG(var);
+    // so as you might have noticed an adress is not that usefull its just a number. we need to know whats in this addess, so
+    // we use dereference operator(*)
+
+    LOG(*ptre);
+
+    // & and * work as opposit
+
+    // so i guess an address isnt just a number it has its own type. Pointer type
+
+    int x{ 5 };
+    int* ptr{ &x }; // initialize ptr with address of variable x
+
+    std::cout << x << '\n';    // print x's value
+    std::cout << *ptr << '\n'; // print the value at the address that ptr is holding (x's address)
+
+    *ptr = 6; // The object at the address held by ptr (x) assigned value 6 (note that ptr is dereferenced here)
+
+    std::cout << x << '\n';
+    std::cout << *ptr << '\n'; // print the value at the address that ptr is holding (x's address)
+
 }
